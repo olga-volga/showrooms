6a78296efc324023a6019f5424dd2cd93bdb7617
olga <yaolmi@yandex.ru>
2022-03-24 14:54:13 +0700

    create html&styles for header

---
+++ dist/script.js
@@ -0,0 +1,11 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+window.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+});
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
